481d0d5fab273e37dc54164368ee1fd9bfb09cc0
CrackSparrow1 <bencevelok@gmail.com>
2023-09-14 16:24:11 +0200

    Első szinkron

--- elso.py
+++ elso.py
@@ -1,1 +1,15 @@
-print('Üdv Solton!')
+print('Üdv \nSolton!', end='')
+print('Szia', 'Richárd', sep=' ')
+
+print('''Ez
+ több 
+          Soltba
+    kerül!''')
+nev = 'rántotthusos szendvics'
+print('Szeretem a', nev, end='et!')
+print('jobbra'.rjust(50))
+
+print('jobbra'.rjust(50, '-'))
+print('balra'.ljust(50, '+'))
+print('mid'.center(50, '/'))
+
